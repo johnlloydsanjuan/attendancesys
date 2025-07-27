5405fa6fd8802fbeabc9b78b2a757667a6bf15f6
johnlloydsanjuan <businesslikesan1@gmail.com>
2025-07-27 09:36:24 +0800

    fix fonts and floating label

--- resources/views/components/input-float-label.blade.php
+++ resources/views/components/input-float-label.blade.php
@@ -4,12 +4,11 @@
     'id' => '',
     'name' => '',
     'class' => '',
-    'placeholder' => '',
     'labelClass' => ''
 ])
 
 
 <div class="input-float-label-holder">
-    <input type="text" class="input-float {{ $class }}" name="{{ $name }}" id="{{ $id }}" placeholder="{{ $placeholder }}">
+    <input type="{{ $type }}" class="input-float {{ $class }}" name="{{ $name }}" id="{{ $id }}" placeholder=" ">
     <label for="{{ $id }}" class="input-float-label {{ $labelClass }}">{{ $inputTitle }}</label>
 </div>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,6 +1,9 @@
 <div class="background">
     <div class="login-content">
-        <x-input-float-label inputTitle="EMAIL" id="test" name="test" />
-        <x-input-float-label inputTitle="PASSWORD" id="test" name="test" />
+        <x-input-float-label inputTitle="Email Address" id="test" name="test" />
+        <x-input-float-label inputTitle="Password" id="test" name="test" />
+        <x-input-float-label inputTitle="haha" id="test" name="test" />
+        <x-input-float-label inputTitle="Pastessword" id="test" name="test" />
+        <x-input-float-label inputTitle="teeest" id="test" name="test" />
     </div>
 </div>
